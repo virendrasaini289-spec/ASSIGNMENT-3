01b8b20cced030bb98dc533bed3ae26959ed0e85
virendrasaini289-spec <virendrasaini289@gmail.com>
2025-11-12 13:54:36 +0530

    Add files via upload

---
+++ assinment 3 task2 .py
@@ -0,0 +1,12 @@
+import math 
+
+
+
+def calculete():
+    a = int(input("Enter a Number :  "))
+    
+    print("Square root : ",math.sqrt(a))
+    print("logarithm  : ",math.log(a))
+    print("Sine : ",math.sin(a))
+
+calculete()
